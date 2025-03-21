f459adb74b2d7421693eb29e875a72d7f994926c
Christopher Pacheco Hernandez <1002047@MY.SCCCD.EDU>
2025-03-20 23:29:22 -0700

    "Implemented readTemperatures functions to load data into array of struct"

--- array_of_structs.cpp
+++ array_of_structs.cpp
@@ -2,27 +2,30 @@
 #include <fstream>
 using namespace std;
 
-// TODO: Step 1 - Define the struct TemperatureRecord
-// It should contain two integer fields: day and temperature.
+struct TemperatureRecord {
+    int day;
+    int temperature;
+};  // TODO: Step 1 - Define the struct TemperatureRecord
+  // It should contain two integer fields: day and temperature.
 
 
 // Constants
 const int MAX_DAYS = 31;
 
 // Function Prototypes
-void readTemperatures(???); // TODO: Fix the parameters
-void printTemperatures(const ???);
-TemperatureRecord findMin(const ???);
-TemperatureRecord findMax(const ???);
-double findAverage(const ???);
+void readTemperatures(int&,TemperatureRecord[]); // TODO: Fix the parameters
+void printTemperatures(const TemperatureRecord[], int);
+//TemperatureRecord findMin(const ???); 
+//TemperatureRecord findMax(const ???);
+double findAverage(const TemperatureRecord[], int);
 
 int main() {
-    // TODO: Step 2 - Declare an array of TemperatureRecord structs (MAX_DAYS size)
+    TemperatureRecord tempDays[MAX_DAYS];// TODO: Step 2 - Declare an array of TemperatureRecord structs (MAX_DAYS size)
     
     int size = 0;  // Actual number of records read
 
     // TODO: Step 3 - Call readTemperatures() to load data from file
-
+    readTemperatures(size, tempDays);
     // TODO: Step 4 - Print the temperatures
 
     // TODO: Step 5 - Compute and display min, max, and average temperature
@@ -32,6 +35,13 @@ int main() {
 
 // TODO: Step 6 - Implement readTemperatures()
 // Read from "temps.txt" and store data in the array
+void readTemperatures(int& size, TemperatureRecord temp[]) {
+    ifstream inputFile("temps.txt");
+    while (size < MAX_DAYS && inputFile >> temp[size].day >> temp[size].temperature) {
+        size++;
+    }
+    inputFile.close();
+}
 
 // TODO: Step 7 - Implement printTemperatures()
 // Print all stored temperatures in a formatted table
